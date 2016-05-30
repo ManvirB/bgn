3119ca970e6ee742f1e7ea5529272e6c725ced0a
andyjojo <andy.wen@sap.com>
2016-05-30 10:40:18 +0800

    fix for multiple method
    implement mul and decryptMul methods

--- src/niclabs/lolhowdoibgn/BGNEncryption.java
+++ src/niclabs/lolhowdoibgn/BGNEncryption.java
@@ -82,18 +82,31 @@ public class BGNEncryption extends IntentService {
 		output.add(aux);
 		return output;
 	}
-	
-	public Element mul(PublicKey PK, Element C, Element D){
-		BigInteger t = BigIntegerUtils.getRandom(PK.getN()); //Make sure product is NOT infinite.
-		Field<?> f = PK.getField();
-		Element Q = PK.getQ();
-		Element output = f.newElement();
-		Element aux = f.newElement();
-		aux.set(PK.doPairing(C, D));
-		output.set(PK.doPairing(Q,Q));
-		output.pow(t);
-		output.mul(aux);
-		return output;
+
+	public Element mul(PublicKey PK, Element C, Element D) {
+		BigInteger t = BigIntegerUtils.getRandom(PK.getN());
+		
+		Element T = PK.doPairing(C, D);
+		
+		Element K = PK.doPairing(PK.getQ(), PK.getQ());
+		K = K.pow(t);
+		return T.mul(K);
+	}
+
+	public String decryptMul(PublicKey PK, BigInteger sk, Element C) {
+		Element PSK = PK.doPairing(PK.getP(), PK.getP());
+		PSK.pow(sk);
+
+		Element CSK = C.duplicate();
+		CSK.pow(sk);
+		Element aux = PSK.duplicate();
+		
+		BigInteger m = new BigInteger("1");
+		while (!aux.isEqual(CSK)) {
+			aux = aux.mul(PSK);
+			m = m.add(BigInteger.valueOf(1));
+		}
+		return m.toString();
 	}
 
 	public String decrypt(PublicKey PK, BigInteger sk, Element C) {
@@ -127,12 +140,15 @@ public class BGNEncryption extends IntentService {
 	public static void main(String[] args) {
 		BGNEncryption b = new BGNEncryption();
 		PublicKey PK = b.gen(32);
-		Element msg = b.encrypt(PK, b.message);
-		Element msg2 = b.encrypt(PK, "111");
-		long t = System.currentTimeMillis();
-		System.out.println(b.decrypt(PK, b.q, b.add(PK, msg, msg2)));
-		System.out.println("Decryption took "
-				+ (System.currentTimeMillis() - t) + " ms");
+		
+		Element msg1 = b.encrypt(PK, "32");
+		Element msg2 = b.encrypt(PK, "15");
+
+		Element add = b.add(PK, msg1, msg2);
+		System.out.println("Addition: " + b.decrypt(PK, b.q, add));
+
+		Element mul = b.mul(PK, msg1, msg2);
+		System.out.println("Mul: " + b.decryptMul(PK, b.q, mul));
 	}
 
 	@Override
